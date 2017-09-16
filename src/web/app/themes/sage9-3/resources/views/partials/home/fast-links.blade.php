a170735772975c7e1cddff9b814789534b3b9329
Phelippe Fonseca <phelippematte@gmail.com>
2017-09-15 21:20:12 -0300

    fix

--- src/web/app/themes/sage9-3/resources/views/partials/home/fast-links.blade.php
+++
@@ -1,7 +0,0 @@
-<section class="fast-links">
-    <div class="container">
-        <a href="#">como ingressar</a>
-        <a href="#">Bolsas e financiamentos</a>
-        <a href="#">Use sua nota do Enem</a>
-    </div>
-</section>
